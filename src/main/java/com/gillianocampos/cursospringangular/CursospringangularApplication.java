2cfbf406575fa05ecaa2bc536afe73ebaa397a22
Gilliano Antunes <gillianoantunes21@yahoo.com>
2022-03-21 16:27:28 -0300

    Projeto criado

---
+++ src/main/java/com/gillianocampos/cursospringangular/CursospringangularApplication.java
@@ -0,0 +1,13 @@
+package com.gillianocampos.cursospringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringangularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gillianocampos/cursospringangular/CursospringangularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gillianocampos.cursospringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringangularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
